fd0dbacf1c000faeb980c8066fca94bc2507cf4e
Brian Firestone <bfirestone2339@gmail.com>
2021-03-28 15:14:21 -0400

    commit tree ab9ef55d47b1d5ac76a75b98e44f5e3ee3625f67

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+const mapToNegativize = (array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(-array[i])
+    }
+    return result;
+}
+
+const mapToNoChange = (array) => {
+    return array;
+}
+
+const mapToDouble = (array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i] * 2)
+    }
+    return result;
+}
+
+const mapToSquare = (array) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(array[i] ** 2)
+    }
+    return result;
+}
+
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    for (let i = 0; i < sourceArray.length; i++) {
+        startingPoint += sourceArray[i];
+    }
+    return startingPoint;
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    let value = true;
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] == false) {
+            value = false;
+        }
+    }
+    return value;
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    let value = false;
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i] == true) {
+            value = true;
+        }
+    }
+    return value;
+}
